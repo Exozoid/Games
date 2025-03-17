092807261fe4dd747b6412695b639b6a44ade625
lucasvlangens@outlook.de <LucasVLangens@outlook.de>
2025-03-17 13:27:16 +0100

    Games that I have mained using only the console appilcation(beginner/intermediate)

---
+++ DiceRollGame.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        UserDiceRoll userDiceRoll = new UserDiceRoll();
+        string userInput = userDiceRoll.GetInput();
+        Console.WriteLine("You entered: " + userInput);
+
+        int diceRoll = userDiceRoll.RandomDiceRoll;
+        Console.WriteLine("You rolled: " + diceRoll);
+    }
+}
+
+public class UserDiceRoll
+{
+    private static Random _random = new Random();
+
+    public string GetInput()
+    {
+        Console.WriteLine("Enter your input:");
+        return Console.ReadLine();
+    }
+
+    public int RandomDiceRoll => _random.Next(1, 7);
+}
